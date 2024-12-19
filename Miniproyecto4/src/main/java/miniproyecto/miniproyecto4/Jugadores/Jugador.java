b5944d8d4639f48041ee1651cd135b6131d32abf
TsunaHalo2001 <joan.villamil@correounivalle.edu.co>
2024-12-19 11:07:54 -0500

    refactored packages

--- Miniproyecto4/src/main/java/miniproyecto/miniproyecto4/Jugadores/Jugador.java
+++ Miniproyecto4/src/main/java/miniproyecto/miniproyecto4/Jugadores/Jugador.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package miniproyecto.Jugadores;
+package miniproyecto.miniproyecto4.Jugadores;
 
-import miniproyecto.Enum.EnumPosicion;
+import miniproyecto.miniproyecto4.Enum.EnumPosicion;
 
 /**
  *
