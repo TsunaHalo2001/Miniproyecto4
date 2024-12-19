8dc1cf88939a88e3de886ce76ac13f5fe1581d3e
TsunaHalo2001 <joan.villamil@correounivalle.edu.co>
2024-12-19 10:17:17 -0500

    Base proyect

---
+++ Miniproyecto4/src/main/java/miniproyecto/miniproyecto4/Miniproyecto4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package miniproyecto.miniproyecto4;
+
+/**
+ *
+ * @author tsuna
+ */
+public class Miniproyecto4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
